b268f7eeac3ef4f3bce51f485d24ef8029a1d2e6
Quiñanola CJ <quinanolacj303@gmail.com>
2026-02-07 10:31:04 +0800

    Implement looping calculator functionality

--- Program.cs
+++ Program.cs
@@ -1,1 +1,52 @@
-﻿//write your console display in here
+using System;
+
+namespace LoopingCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isRunning = true;
+
+            while (isRunning)
+            {
+                Console.Write("Enter first number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine())
+                
+                Console.Write("Enter second number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                char operation;
+
+                while (true)
+                {
+                    Console.Write("Choose operation (+, -, *, /, %, =): ");
+                    operation = Convert.ToChar(Console.ReadLine());
+
+                    if (Calculator.IsValidOperation(operation))
+                        break;
+
+                    Console.WriteLine("Incorrect Operation Used, please try again");
+                }
+
+                
+                if (operation == '=')
+                {
+                    isRunning = false;
+                    break;
+                }
+
+                object result = Calculator.Calculate(num1, num2, operation);
+
+                if (result != null)
+                {
+                    Console.WriteLine("Result: " + result);
+                }
+
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Exiting Program...");
+        }
+    }
+}
